aec6d3cf10e0c067ef038a01d828bc46a0b85aa0
Isaac Ramirez <isaacravi88@gmail.com>
2026-08-06 20:12:21 +0200

    feat: añadir logica y utilidades funciones

--- src/functional_utils.py
+++ src/functional_utils.py
@@ -1,1 +1,14 @@
-# FUNCTIONAL_UTILS
+from functools import reduce
+
+
+def filter_sales_by_category(sales, category):
+    return list(filter(lambda sale: sale.category == category, sales))
+
+def filter_sales_by_client(sales, client_id):
+    return list(filter(lambda sale: sale.client_id == client_id, sales))
+
+def sales_to_dict(sales):
+    return list(map(lambda sale: sale.to_dict(),sales))  # noqa: C417
+
+def total_sales_amount(sales):
+    return reduce(lambda total, sale: total + sale.amount, sales, 0.0)
