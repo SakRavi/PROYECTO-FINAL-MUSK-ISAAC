241e383101f6abf92bc09404ff5dcb3ea6ae1f9c
Isaac Ramirez <isaacravi88@gmail.com>
2026-08-06 16:20:59 +0200

    feat: logica de client/ sale

--- src/client.py
+++ src/client.py
@@ -2,7 +2,13 @@
 
 class Client:
 
-    def __init__(self, client_id: int, name: str, country: str, signup_date: str):
+    def __init__(
+        self,
+        client_id: int,
+        name: str,
+        country: str,
+        signup_date: str,
+        ):
         
         self.client_id = client_id
         self.name = name
@@ -14,16 +20,9 @@ class Client:
             "client_id": self.client_id,
             "name": self.name,
             "country": self.country,
-            "signup_date": self.signup_date
+            "signup_date": self.signup_date,
         }
 
 # PRE_TESTING
-if __name__ == "__main__":
-    client = Client(
-        client_id=1,
-        name="Alice",
-        country="Spain",
-        signup_date="2022-03-15",
-    )
-    print(client.to_dict())
+
 
--- src/sale.py
+++ src/sale.py
@@ -1,1 +1,32 @@
 # CLASS SALE
+
+class Sale:
+    
+    def __init__ (
+        self,
+        sale_id:int,
+        client_id:int,
+        product:str,
+        category:str,
+        amount:int,
+        date:str,
+        ):
+        
+        self.sale_id = sale_id
+        self.client_id = client_id
+        self.product = product
+        self.category = category
+        self.amount = amount
+        self.date = date
+        
+    def to_dict(self):
+        return {
+            "sale_id": self.sale_id,
+            "client_id": self.client.id,
+            "product": self.product,
+            "category": self.category,
+            "amount": self.amount,
+            "date": self.date,
+        }
+        
+# PRE TESTING
